3b6c7699c51b0151c9155563a56f6f3bd07b6795
NotAVeryHappyPerson <rithviknadendla@gmail.com>
2024-01-22 19:29:11 -0500

    Started work on the absolute encoder to use in the pivot subsystem

--- src/main/java/frc/robot/subsystems/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -8,6 +8,7 @@ import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.extension.ShooterLevel;
@@ -18,10 +19,17 @@ public class Pivot extends SubsystemBase {
 
   /** Creates a new Pivot. */
    CANSparkMax pivotMotor;
+
+   // Initializes a duty cycle encoder on DIO pins 0
+   DutyCycleEncoder encoder;
+   
+   
+   
     public static ShooterLevel shooterLevel;
   public Pivot() {
     pivotMotor = SparkMax.createDefaultCANSparkMax(17);
-     shooterLevel = ShooterLevel.DEFAULT;
+    shooterLevel = ShooterLevel.DEFAULT;
+    encoder = new DutyCycleEncoder(0);
   }
 
 /** makes pivot motor move */
@@ -49,6 +57,7 @@ public class Pivot extends SubsystemBase {
   public void netTbls(SendableBuilder builder) {
     builder.addStringProperty("Angle", this::returnShooterLevel, null);
   }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
@@ -67,22 +76,32 @@ public class Pivot extends SubsystemBase {
     {
       // Angled towards the Human Player for loading 
       case LOAD:
+        encoder.setDutyCycleRange(1.0/1024.0, 1023.0/1024.0);
+        encoder.setDistancePerRotation(0.0);
         System.out.println("Load");
         break;
       case FEED:
+        encoder.setDutyCycleRange(1.0/1024.0, 1023.0/1024.0);
+        encoder.setDistancePerRotation(30.0);
         System.out.println("Feed");
         break;
       // Lays shooter flat, deafult position. 
       case DEFAULT:
-        System.out.println("Deafult");
+        encoder.setDutyCycleRange(1.0/1024.0, 1023.0/1024.0);
+        encoder.setDistancePerRotation(60.0);
+        System.out.println("Default");
         break;
       // Angles shooter upwards
       case AMP:
         System.out.println("Amp");
+         encoder.setDutyCycleRange(1.0/1024.0, 1023.0/1024.0);
+        encoder.setDistancePerRotation(90.0);
         break;
       // Less steeper then Amp but more steeper then deafult, used for shooting
       case SPEAKER:
         System.out.println("Speaker");
+        encoder.setDutyCycleRange(1.0/1024.0, 1023.0/1024.0);
+        encoder.setDistancePerRotation(120.0);
         break;
   
       default:
