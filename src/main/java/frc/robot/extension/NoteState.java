93b7de4f2fb23310c5141798f2d44f7a5599fa0e
kyle <kyle@trex4935.com>
2024-01-25 19:43:35 -0500

    Made new state machines in the intake and rollers

--- src/main/java/frc/robot/extension/NoteState.java
+++ src/main/java/frc/robot/extension/NoteState.java
@@ -6,12 +6,12 @@ package frc.robot.extension;
 
 /** Add your docs here. */
 public enum NoteState {
-    FIELD,
-    INTAKE,
-    GRABBED,
-    CONTROL,
-    STORAGE,
-    LOADING,
-    SPEAKER,
-    AMP
+    FIELD, // Note is on the field and we are ignoring it
+    INTAKE, // Note is on the field but is gonna be picked up
+    GRABBED, // Note has been intaked, Intake sensor sees note
+    CONTROL, // Note is inside robot, intake sensor no longer sees note
+    STORAGE, // Note is stationary inside robot, magazine sensor sees note
+    SPEAKER,  // Note is shot out towards speaker
+    AMPLOADING, // Note is moving to the amp drop position
+    AMP // Note is dropped into the amp
 }
