0de67afcd18d6b3dd8c1fef0c222961b0e0fdee0
NotAVeryHappyPerson <rithviknadendla@gmail.com>
2024-01-26 18:14:22 -0500

    intake added

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,29 +5,54 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
   // DoubleSolenoid doublesolenoid;
   Solenoid solenoid;
+  PowerDistribution PDH;
+
+  private final Compressor m_compressor = new Compressor(PneumaticsModuleType.CTREPCM);
   /** Creates a new IntakeLift. */
   public Intake() {
     // doublesolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH,33, 44);
     solenoid = new Solenoid(PneumaticsModuleType.REVPH, 22);
-
+    PDH = new PowerDistribution(1, ModuleType.kRev);
   }
   public void switchIntake() {
     solenoid.toggle();
   }
-
+  public void PDH_on(){
+    PDH.setSwitchableChannel(true);
+  }
+  public void PDH_off(){
+    PDH.setSwitchableChannel(false);
+  }
 
   public boolean getIntakeState() {
     return solenoid.get();
   }
+  
+  public double sensePSI (){
+    return m_compressor.getPressure();
+  }
+public boolean OnOff_Compressor (){
 
-
+return m_compressor.isEnabled();
+}
+public void PDH_Toggle(){
+  if (OnOff_Compressor()){
+    PDH_on();}
+  else{
+    PDH_off();
+  }
+}
+  
 
   public void initSendable(SendableBuilder builder){
     builder.addBooleanProperty("Intake Dropped", this::getIntakeState, null);
