2f3d6ec10d4e6e6c03ecf6a23d2871c05d059c5d
nugget <nugget@trex4935.com>
2024-03-04 18:32:50 -0500

    Created manual movement with joysticks

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.AlignWithPID;
 import frc.robot.extension.Alignment;
 import frc.robot.extension.NoteState;
@@ -47,7 +48,7 @@ public class RobotContainer {
 
   // Setting up bindings for necessary control of the swerve drive platform
   private final CommandXboxController joystick = new CommandXboxController(0); // My joystick
-	
+
   public final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain; // My drivetrain
 
   // Make sure things are field centric for swerve
@@ -61,12 +62,11 @@ public class RobotContainer {
 
   // Swerve Telemetry
   private final Telemetry logger = new Telemetry(MaxSpeed, drivetrain);
-	
-	// Alternate align command
-	// TODO: Tune offset values
-	private final AlignWithPID align = new AlignWithPID(drivetrain, () -> 
-	getTargetPose(Alignment.speakerAprilTag, Alignment.speakerOffset), false, false);
 
+  // Alternate align command
+  // TODO: Tune offset values
+  private final AlignWithPID align = new AlignWithPID(drivetrain,
+      () -> getTargetPose(Alignment.speakerAprilTag, Alignment.speakerOffset), false, false);
 
   // Creates the autoChooser to use in the sendables
   private final SendableChooser<Command> autoChooser;
@@ -81,7 +81,7 @@ public class RobotContainer {
     rollers.setDefaultCommand(rollers.run(() -> rollers.rollerSwitch()));
     shooter.setDefaultCommand(shooter.run(() -> shooter.shooterSwitch()));
     pivot.setDefaultCommand(pivot.run(() -> pivot.pivotSwitch()));
-		ledControl.setDefaultCommand(ledControl.run(() -> ledControl.ledSwitch()));
+    ledControl.setDefaultCommand(ledControl.run(() -> ledControl.ledSwitch()));
 
     drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
 
@@ -101,23 +101,25 @@ public class RobotContainer {
     joystick.b().whileTrue(drivetrain
         .applyRequest(() -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
 
-		// reset the field-centric heading on left bumper press
-		joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
+    // reset the field-centric heading on left bumper press
+    joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
 
+    // Up on the D-pad automatically aligns to the speaker
+    joystick.povUp().whileTrue(drivetrain.alignWithPathPlanner(Alignment.speakerAprilTag, Alignment.speakerOffset)
+        .andThen(drivetrain.applyRequest(() -> brake)));
+    // Down on the D-pad automatically aligns to the source
+    joystick.povDown().whileTrue(drivetrain.alignWithPathPlanner(
+        Alignment.sourceAprilTag, Alignment.sourceOffset).andThen(drivetrain.applyRequest(() -> brake)));
+    // Left on the D-pad automatically aligns to the amp
+    joystick.povLeft().whileTrue(drivetrain.alignWithPathPlanner(Alignment.ampAprilTag, Alignment.ampOffset)
+        .andThen(drivetrain.applyRequest(() -> brake)));
+    // Right on the D-pad automatically aligns to the stage
+    joystick.povRight().whileTrue(drivetrain.alignWithPathPlanner(
+        drivetrain.getState().Pose.nearest(Alignment.stageAprilTags), Alignment.stageOffset)
+        .andThen(drivetrain.applyRequest(() -> brake)));
 
-		// Up on the D-pad automatically aligns to the speaker
-		joystick.povUp().whileTrue(drivetrain.alignWithPathPlanner(Alignment.speakerAprilTag, Alignment.speakerOffset).andThen(drivetrain.applyRequest(() -> brake)));
-		// Down on the D-pad automatically aligns to the source
-		joystick.povDown().whileTrue(drivetrain.alignWithPathPlanner(
-			Alignment.sourceAprilTag, Alignment.sourceOffset).andThen(drivetrain.applyRequest(() -> brake)));
-		// Left on the D-pad automatically aligns to the amp
-		joystick.povLeft().whileTrue(drivetrain.alignWithPathPlanner(Alignment.ampAprilTag, Alignment.ampOffset).andThen(drivetrain.applyRequest(() -> brake)));
-		// Right on the D-pad automatically aligns to the stage
-		joystick.povRight().whileTrue(drivetrain.alignWithPathPlanner(
-			drivetrain.getState().Pose.nearest(Alignment.stageAprilTags), Alignment.stageOffset).andThen(drivetrain.applyRequest(() -> brake)));
-
-		// The menu button aligns using a PID
-		joystick.start().whileTrue(align);
+    // The menu button aligns using a PID
+    joystick.start().whileTrue(align);
 
     // Helps run the simulation
     if (Utils.isSimulation()) {
@@ -127,14 +129,14 @@ public class RobotContainer {
 
     // Buttton 8 runs pivot towards battery
     operatorTestButton.button(8)
-        .whileTrue(climber.runEnd(() -> climber.setClimberMotors(-0.5), () -> climber.stopClimberMotors()));
+        .whileTrue(pivot.run(() -> pivot.manualPivotForward()));
 
     // Button 9 changes state to ground intake
     operatorTestButton.button(9).onTrue(rollers.runOnce(() -> rollers.changeNoteState(NoteState.GROUNDINTAKE)));
 
     // Button 10 runs pivot towards force field
     operatorTestButton.button(10)
-        .whileTrue(climber.runEnd(() -> climber.setClimberMotors(0.5), () -> climber.stopClimberMotors()));
+        .whileTrue(pivot.run(() -> pivot.manualPivotBackward()));
 
     // Button 11 changes state to field
     operatorTestButton.button(11).onTrue(rollers.runOnce(() -> rollers.returnToField()));
@@ -146,14 +148,14 @@ public class RobotContainer {
     // Button 13 changes state to speaker
     operatorTestButton.button(13).onTrue(rollers.runOnce(() -> rollers.changeNoteState(NoteState.SPEAKER)));
 
-		// Button 14 changes state to source
+    // Button 14 changes state to source
     operatorTestButton.button(14).onTrue(rollers.runOnce(() -> rollers.changeNoteState(NoteState.SOURCE)));
 
   }
 
   // Sendables to put autoChooser and Pivot Angle in the SmartDashboard.
   public RobotContainer() {
-		Alignment.updateAprilTagTranslations();
+    Alignment.updateAprilTagTranslations();
     configureBindings();
     SmartDashboard.putData(dustpan);
     SmartDashboard.putData(rollers);
@@ -163,18 +165,18 @@ public class RobotContainer {
 
     autoChooser = AutoBuilder.buildAutoChooser();
     SmartDashboard.putData("Auto Mode", autoChooser);
-	}
+  }
 
-	public Pose2d getTargetPose(Pose2d aprilTagPose, double[] offsetArray) {
-		// Creates an offset pose from the offset array
-		 Pose2d pose2dOffset = new Pose2d(offsetArray[0], offsetArray[1], Rotation2d.fromDegrees(offsetArray[2]));
-		// Gets target values from the tag poses and the offset
-		double targetX = aprilTagPose.getX() + pose2dOffset.getX();
-		double targetY = aprilTagPose.getY() + pose2dOffset.getY();
-		Rotation2d targetTheta = pose2dOffset.getRotation();
-		// Makes a target pose
-		Pose2d targetPose = new Pose2d(targetX, targetY, targetTheta);
-		return targetPose;
+  public Pose2d getTargetPose(Pose2d aprilTagPose, double[] offsetArray) {
+    // Creates an offset pose from the offset array
+    Pose2d pose2dOffset = new Pose2d(offsetArray[0], offsetArray[1], Rotation2d.fromDegrees(offsetArray[2]));
+    // Gets target values from the tag poses and the offset
+    double targetX = aprilTagPose.getX() + pose2dOffset.getX();
+    double targetY = aprilTagPose.getY() + pose2dOffset.getY();
+    Rotation2d targetTheta = pose2dOffset.getRotation();
+    // Makes a target pose
+    Pose2d targetPose = new Pose2d(targetX, targetY, targetTheta);
+    return targetPose;
   }
 
   /** Returns the note state cycle */
