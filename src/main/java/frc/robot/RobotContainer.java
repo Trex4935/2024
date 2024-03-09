a99c031e7a37741cbf7f07914265f5f3d116059e
nugget <nugget@trex4935.com>
2024-03-09 17:41:11 -0500

    Comment removed

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -238,6 +238,5 @@ public class RobotContainer {
   /** Runs autoChooser :) */
   public Command getAutonomousCommand() {
     return autoChooser.getSelected();
-    // return new PathPlannerAuto("1 Piece Auto(Mid)");
   }
 }
